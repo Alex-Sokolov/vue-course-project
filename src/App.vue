fe303f5b93eaba07b59d9eb4f96f98d150a8d4e2
Alex-Sokolov <Alex-Sokolov@users.noreply.github.com>
2017-05-21 14:13:41 +0300

    Добавлена навигация

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,34 @@
 <template>
   <div id="app">
 
-    <div class="container">
-
-      <div class="hello">
-        <router-link to="/user">Открыть пользователя</router-link>
-        <router-link to="/list">Открыть список пользователей</router-link>
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">
+            Vue.js course project
+          </a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="">
+              <router-link to="/user">Открыть пользователя</router-link>
+            </li>
+            <li class="">
+              <router-link to="/list">Открыть список пользователей</router-link>
+            </li>
+          </ul>
+        </div>
+        <!--/.nav-collapse -->
       </div>
+    </nav>
 
+    <div class="container">
       <router-view></router-view>
     </div>
   </div>
