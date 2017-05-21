f7a0af28868c7d982ed7f9cdaf5570d037f38c52
Alex-Sokolov <Alex-Sokolov@users.noreply.github.com>
2017-05-21 14:13:54 +0300

    Добавлен набросок таблицы

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,6 +1,19 @@
 <template>
   <div>
     {{ test }}
+
+    <table class="table table-bordered">
+      <tbody>
+        <tr>
+          <td>
+            ФИО пользователей
+          </td>
+          <td>
+            Даты рождения
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
