0144f9af7b28ba29ed581c5f35d327d428ed53a2
Alex Sokolov <Alex-Sokolov@users.noreply.github.com>
2017-06-02 10:15:15 +0300

    WIP

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>{{ title }}</h1>
 
     <router-link to="/user/add">Добавить пользователя</router-link>
     <router-link to="/list">Открыть список пользователей</router-link>
@@ -14,29 +14,8 @@ export default {
   name: 'hello',
   data() {
     return {
-      msg: 'Vue.js course project',
+      title: 'Vue.js course project',
     };
   },
 };
 </script>
-
-<style scoped>
-h1,
-h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-</style>
